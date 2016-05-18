d05245126a2dccb28048ec549c632d5e5309c02e
Leela Krishna Devalla <krishnadevalla@outlook.com>
2016-05-18 09:14:03 -0400

    Removed unused Usings

--- PhoneApp1/MainPage.xaml.cs
+++ PhoneApp1/MainPage.xaml.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
-using PhoneApp1.Resources;
 using System.Threading;
 using System.Collections.ObjectModel;
 
